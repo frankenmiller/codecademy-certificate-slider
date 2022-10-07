6e5c1ba2f0cc0591717cde07b8ead181f23c809d
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-10-07 20:56:59 +0200

    Hamburger menu added, HREF values set and linear gradient for hamburger menu created, social icons centered

--- main.js
+++ main.js
@@ -34,3 +34,13 @@ var swiper = new Swiper('.swiper', {
     clickable: true,
   }
 });
+
+window.onload = function () {
+  const menu_button = document.querySelector('.hamburger');
+  const mobile_menu = document.querySelector('.mobile-nav');
+
+  menu_button.addEventListener('click', function () {
+      menu_button.classList.toggle('is-active');
+      mobile_menu.classList.toggle('is-active');
+  });
+}
