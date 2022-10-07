e15bd61228c6aebe82480b41b662429de7067a72
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-10-07 14:47:25 +0200

    Adj'd pagination values, for speed bullet position and bullet color

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 var swiper = new Swiper('.swiper', {
   effect: 'coverflow',
   grabCursor: true,
+  paginationClickable: true,
+  speed: 2500,
   centeredSlides: true,
   slidesPerView: 'auto',
   coverflowEffect: {
-    rotate: 75,
-    stretch: 0,
+    rotate: 25,
+    stretch: -200,
     depth: 500,
     modifier: 1,
     slideShadows: true,
   },
   loop: true,
   autoplay: {
-    delay: 1500,
+    delay: 3000,
+    transition: 2000,
     disableOnInteraction: false,
   },
   pagination: {
     el: '.swiper-pagination',
+    clickable: true
   }
 });
