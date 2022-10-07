5a58c3d49569ee5604a72f5417081f88039ce239
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-10-07 14:28:40 +0200

    Added reflection and autoplay feature

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ var swiper = new Swiper('.swiper', {
   centeredSlides: true,
   slidesPerView: 'auto',
   coverflowEffect: {
-    rotate: 80,
+    rotate: 75,
     stretch: 0,
     depth: 500,
     modifier: 1,
     slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
   }
 });
