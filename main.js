5d5b71e0df97630465b22eff17c31c442d3848b4
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-10-07 13:49:12 +0200

    Added png img's for all certificates, all tested and displaying well

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var swiper = new Swiper('.swiper', {
   centeredSlides: true,
   slidesPerView: 'auto',
   coverflowEffect: {
-    rotate: 20,
+    rotate: 80,
     stretch: 0,
-    depth: 200,
+    depth: 500,
     modifier: 1,
     slideShadows: true,
   }
