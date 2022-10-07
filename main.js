b3bc13e7d072884fd45789bd391c4dedce4a0c1f
Frankenmiller <Dr.Frankenmiller@gmail.com>
2022-10-07 13:22:43 +0200

    Added png img's for blockchain and SQL

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-import Swiper from 'https://unpkg.com/browse/swiper@8.4.3/swiper-bundle.esm.js';
-import 'https://unpkg.com/browse/swiper@8.4.3/swiper-bundle.css';            
 var swiper = new Swiper('.swiper', {
   effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: 'auto',
   coverflowEffect: {
-    rotate: 50,
+    rotate: 20,
     stretch: 0,
-    depth: 100,
+    depth: 200,
     modifier: 1,
     slideShadows: true,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  }
 });
